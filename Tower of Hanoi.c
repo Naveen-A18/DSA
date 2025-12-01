5efa21e91f1bc28e81ccd0836ef16a3886e9ca01
Naveen A <2006naveen18@gmail.com>
2025-12-01 15:44:38 +0530

    Add files via upload

---
+++ Tower of Hanoi.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void tower(int n, char source, char temp,char destination)
+{
+	if(n == 1)
+		return;
+	tower(n-1, source, destination, temp);
+	printf("\nMove disc %d from %c to %c", n, source, destination);
+	tower(n-1, temp, source, destination);
+}
+void main()
+{
+int n;
+clrscr();
+printf("\nEnter the number of discs: \n");
+scanf("%d", &n);
+tower(n, 'A', 'B', 'C');
+printf("\n\nTotal Number of moves are: %d", (int)pow(2,n)-1);
+getch();
+}
